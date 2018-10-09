3e0c47e747182df458e1e0ddd00c56912b146775
Marc Schweikert <schweikertm@udev.local>
2018-10-09 11:03:42 -0400

    Allow force rebuild

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -o nounset
 
-docker build -t docker-xfce-vnc:latest .
+args=""
+if [[ "${1:-}" == "-f" ]]; then
+    args="--force-rm --no-cache"
+fi
+
+docker build ${args} -t docker-xfce-vnc:latest .
