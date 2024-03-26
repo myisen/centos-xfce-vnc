55f45e0994a36d8ae2771194215e430edc93aa6c
myisen <myisen@163.com>
2024-03-26 23:08:25 +0800

    change build.sh to anolis

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ if [[ "${1:-}" == "-f" ]]; then
     args="--force-rm --no-cache"
 fi
 
-docker build ${args} -t centos-xfce-vnc:latest .
+# docker build ${args} -t centos-xfce-vnc:latest .
+docker build ${args} -t anolis-xfce-vnc:7.9 .
